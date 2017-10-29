72e7b20ee9f9cb7385ef381609e5109857849a10
waiphyoehtun <waiphyoe.awitd@gmail.com>
2017-10-29 14:27:56 +0630

    NewFormAdd

--- TestGit/Form2.cs
+++ TestGit/Form2.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace TestGit
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form2 f = new Form2();
-            f.ShowDialog();
         }
     }
 }
